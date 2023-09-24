0091219dcf4d5ce78c598328cda08b5d4502680d
aadv1k <aadv1k@outlook.com>
2023-09-24 13:56:42 +0530

    Add tests for the core data aggregator

---
+++ packages/core/tests/aggregator.test.ts
@@ -0,0 +1,7 @@
+describe("Tests for the Aggregator", () => {
+    test.todo("Should get the top 10 global headlines");
+    test.todo("Should get the latest news specified by the region 'IN' ");
+    test.todo("Should get the latest news specified by the region 'US' ");
+    test.todo("Should exclude the specified news sites");
+    test.todo("Should exclude the news if headlines contains keywords");
+})
