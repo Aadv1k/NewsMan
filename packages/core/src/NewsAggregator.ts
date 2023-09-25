ad375c6fc3933a5aed07b685b47e1f60b1f6e859
aadv1k <aadv1k@outlook.com>
2023-09-25 15:08:54 +0530

    Refactor the logic for the aggregator, remove redundant files

--- packages/core/src/NewsAggregator.ts
+++ packages/core/src/NewsAggregator.ts
@@ -1,62 +1,54 @@
-import * as draco from "dracoql";
+import * as draco from 'dracoql';
 import assert from 'assert';
+import path from 'path';
+import { promises as fs } from 'fs';
 
-import * as utils from "./utils";
+import * as utils from './utils';
 
-import path from "path";
-import fs from "fs/promises";
-
-export interface News {
-    title: string;
-    description?: string | null;
-    coverUrl?: string;
-    url: string;
+interface News {
+  title: string;
+  description?: string | null;
+  coverUrl?: string;
+  url: string;
 }
-
-export interface ProviderConfig {
-    maxItems?: number;
-    excludeSites?: Array<string>;
-}
-
-const HEADLINE_SOURCE_PATH = "../dql/headlines";
-const DQL_EXTENSION = ".dql";
 
 interface DQLTextNode {
-    type: "TextNode";
-    text: string;
+  type: 'TextNode';
+  text: string;
 }
 
 interface DQLHtmlElement {
-    tag: string;
-    attributes: {
-        href?: string;
-        src?: string;
-    };
-    children: Array<DQLHtmlElement | DQLTextNode>;
+  tag: string;
+  attributes: {
+    href?: string;
+    src?: string;
+  };
+  children: Array<DQLHtmlElement | DQLTextNode>;
 }
 
 interface DQLHtmlObject {
-    type: 'JSON';
-    value: DQLHtmlElement;
+  type: 'JSON';
+  value: DQLHtmlElement;
 }
 
 interface DQLFlatObject {
-    headings: Array<string>;
-    links: Array<{
-        href: string;
-        text: string;
-    }>;
-    paragraphs: Array<string>;
-    images: Array<{
-        src: string;
-        alt: string;
-    }>;
+  headings: Array<string>;
+  links: Array<{
+    href: string;
+    text: string;
+  }>;
+  paragraphs: Array<string>;
+  images: Array<{
+    src: string;
+    alt: string;
+  }>;
 }
 
 function serializeDQLObjectToObject(element: DQLHtmlElement): DQLFlatObject {
-  const containerTagSet = new Set(["div", "article", "span", "li", "ul", "ol"]);
-  if (!containerTagSet.has(element.tag))
+  const containerTagSet = new Set(['div', 'article', 'span', 'li', 'ul', 'ol']);
+  if (!containerTagSet.has(element.tag)) {
     throw new Error(`ERROR: serializeDQLObjectToObject expected a container tag, got ${element.tag}`);
+  }
 
   const flatObject: DQLFlatObject = {
     headings: [],
@@ -66,18 +58,16 @@ function serializeDQLObjectToObject(element: DQLHtmlElement): DQLFlatObject {
   };
 
   function processElement(node: any) {
-    if (node.tag === "h1" || node.tag === "h2" || node.tag === "h3" || node.tag === "h4" || node.tag === "h5" || node.tag === "h6") {
-      flatObject.headings.push((node.children[0] as DQLTextNode)?.text || "");
-    } else if (node.tag === "a" && node.attributes?.href) {
-      if (node.children[0]?.type === "TextNode") {
+    if (node.tag === 'h1' || node.tag === 'h2' || node.tag === 'h3' || node.tag === 'h4' || node.tag === 'h5' || node.tag === 'h6') {
+      flatObject.headings.push((node.children[0] as DQLTextNode)?.text || '');
+    } else if (node.tag === 'a' && node.attributes?.href) {
+      if (node.children[0]?.type === 'TextNode') {
         flatObject.links.push({ href: node.attributes.href, text: node.children[0].text });
-      } else if (node.children[0]?.tag === "img") {
-          flatObject.images.push({ src: node.children[0].attributes.src, alt: node.children[0].attributes.alt });
+      } else if (node.children[0]?.tag === 'img') {
+        flatObject.images.push({ src: node.children[0].attributes.src, alt: node.children[0].attributes.alt });
       }
-
-
-    } else if (node.tag === "p") {
-      flatObject.paragraphs.push((node.children[0] as DQLTextNode)?.text || "");
+    } else if (node.tag === 'p') {
+      flatObject.paragraphs.push((node.children[0] as DQLTextNode)?.text || '');
     }
 
     if (node.children) {
@@ -91,53 +81,74 @@ function serializeDQLObjectToObject(element: DQLHtmlElement): DQLFlatObject {
   return flatObject;
 }
 
-export default class NewsProvider {
-    async fetchHeadlines(config: ProviderConfig): Promise<News[]> {
-        const dirPath = path.join(__dirname, HEADLINE_SOURCE_PATH);
-        const files = await fs.readdir(dirPath);
-        assert.notStrictEqual(files.length, 0, `At least one DracoQL file is expected in ${HEADLINE_SOURCE_PATH}`);
-        let newsArticles: News[] = [];
+export class NewsProvider {
+  async fetchHeadlines(config: ProviderConfig): Promise<News[]> {
+    const dirPath = path.join(__dirname, HEADLINE_SOURCE_PATH);
+    const files = await fs.readdir(dirPath);
+    assert.notStrictEqual(files.length, 0, `At least one DracoQL file is expected in ${HEADLINE_SOURCE_PATH}`);
 
-        for (const fileName of files) {
-            if (!fileName.endsWith(DQL_EXTENSION)) continue;
-            const filePath = path.join(dirPath, fileName);
-            const fileContent = await fs.readFile(filePath, "utf-8");
+    const newsArticles: News[] = [];
 
-            let dracoLexer, dracoParser, dracoInterpreter;
+    for (const fileName of files) {
+      if (newsArticles.length >= (config.maxItems ?? MAX_DEFAULT_HEADLINES)) {
+        break;
+      }
 
-            try {
-                dracoLexer = new draco.lexer(fileContent);
-                dracoParser = new draco.parser(dracoLexer.lex());
-                dracoInterpreter = new draco.interpreter(dracoParser.parse());
-            } catch (error: any) {
-                throw new Error("ERROR: unable to parse DracoQL due to error: " + error.message);
-            }
+      if (!utils.isDQLFileNameValid(fileName)) {
+        continue;
+      }
 
-            try {
-                await dracoInterpreter.run();
-            } catch (error: any) {
-                throw new Error("ERROR: unable to fetch headlines due to DracoQL error: " + error.message);
-            }
+      const parsedFileName = utils.parseDQLFileName(fileName);
 
-            const dracoNS = dracoInterpreter.NS;
+      if (config.region && config.region !== parsedFileName.region) {
+        continue;
+      }
 
-            for (let key in dracoNS) {
-                if (dracoNS[key]?.type === "HTML" || !dracoNS?.[key]) continue;
+      const filePath = path.join(dirPath, fileName);
+      const fileContent = await fs.readFile(filePath, 'utf-8');
 
-                (dracoNS[key] as any).value.children.forEach((elem: any) => {
-                    if (elem?.type === "TextNode") return;
-                    const news = serializeDQLObjectToObject(elem)
+      let dracoLexer, dracoParser, dracoInterpreter;
 
-                    newsArticles.push({
-                        title: news.headings?.[0] || news.links[0].text || news.images[0].alt,
-                        url: utils.isRelativeURL(news.links[0].href) ?
-                            `${utils.convertKebabCaseToURL(fileName)}/${news.links[0].href}` : news.links[0].href,
-                        description: news.paragraphs?.[0] || null,
-                        coverUrl: news.images?.[0].src,
-                    }); 
-                })
-            }
+      try {
+        dracoLexer = new draco.lexer(fileContent);
+        dracoParser = new draco.parser(dracoLexer.lex());
+        dracoInterpreter = new draco.interpreter(dracoParser.parse());
+      } catch (error: any) {
+        throw new Error(`ERROR: unable to parse DracoQL due to error: ${error.message}`);
+      }
+
+      try {
+        await dracoInterpreter.run();
+      } catch (error: any) {
+        throw new Error(`ERROR: unable to fetch headlines due to DracoQL error: ${error.message}`);
+      }
+
+      const dracoNS = dracoInterpreter.NS;
+
+      for (let key in dracoNS) {
+        if (dracoNS[key]?.type === 'HTML' || !dracoNS?.[key]) {
+          continue;
         }
-        return newsArticles;
+
+        (dracoNS[key] as any).value.children.forEach((elem: any) => {
+          if (elem?.type === 'TextNode') {
+            return;
+          }
+
+          const news = serializeDQLObjectToObject(elem);
+
+          newsArticles.push({
+            title: news.headings?.[0] || news.links[0]?.text || news.images[0]?.alt,
+            url: utils.isRelativeURL(news.links[0].href) ? `${parsedFileName.domain}/${news.links[0].href}` : news.links[0].href,
+            description: news.paragraphs?.[0] || null,
+            coverUrl: news.images?.[0]?.src,
+          });
+        });
+      }
     }
+    return newsArticles;
+  }
 }
+
+export default NewsProvider;
+
--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,18 +1,35 @@
-// nytimes-com.dql -> nytimes.com
-export function convertKebabCaseToURL(name: string): string {
-  if (!name || name.indexOf('-') === -1) throw new Error('Invalid input: Name must be a kebab-case string with at least one hyphen.');
-  const [filename, ext] = name.split('.');
-  const [top, rest] = filename.split('-');
-  if (!top || !rest) throw new Error('Invalid input: Name must have a non-empty left and right part separated by a hyphen.');
-  const urlFriendlyName = `https://${top}.${rest}`;
-  try {
-    new URL(urlFriendlyName);
-    return urlFriendlyName;
-  } catch {
-    throw new Error('Invalid input: Resulting URL is not valid.');
-  }
+export function isRelativeURL(url: string): boolean {
+  return !/^(https?:\/\/|\/\/)/.test(url);
 }
 
-export function isRelativeURL(url: string): boolean {
-    return !/^(https?:\/\/|\/\/)/.test(url);
+export interface DQLFileName {
+  language: string;
+  region: NewsRegion;
+}
+
+export enum NewsRegion {
+  US = 'us',
+  IN = 'in',
+}
+
+export function isDQLFileNameValid(filename: string): boolean {
+  const pattern = /^[a-zA-Z0-9-]+_[a-z]+_[a-z]+\.dql$/;
+  return pattern.test(filename);
+}
+
+export function parseDQLFileName(filename: string): DQLFileName {
+  const [domain, region] = filename.split('_');
+
+  const fixedDomain = `https://${domain.replace('-', '.')}`;
+
+  new URL(fixedDomain);
+
+  if (!(region in NewsRegion)) {
+    throw new Error(`Invalid region: ${region}`);
+  }
+
+  return {
+    domain: fixedDomain,
+    region,
+  };
 }
