9ff631ee5ea48c43975eab676e6b38becd8c8f87
aadv1k <aadv1k@outlook.com>
2023-09-28 12:11:10 +0530

    Update tests

--- packages/core/tests/aggregator.test.ts
+++ packages/core/tests/aggregator.test.ts
@@ -1,7 +1,13 @@
 import NewsProvider from "../src/NewsAggregator";
+import path from "node:path";
 
 describe("Tests for the Aggregator", () => {
-    const provider = new NewsProvider();
+    const provider = new NewsProvider(
+        path.join(__dirname, "../dql/headlines"), // headline dir path
+        undefined, // everything dir path 
+        "in" // default country code
+    );
+
     test("Should get the top 10 global headlines", async () => {
         const data = await provider.fetchHeadlines({ maxItems: 10 });
         expect(data.length).toBeGreaterThan(0);
