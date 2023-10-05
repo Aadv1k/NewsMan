e31cbfea5745c178894f791de7c7644dbf3af5e4
aadv1k <aadv1k@outlook.com>
2023-10-05 12:40:56 +0530

    Further work on the landing pageh

--- packages/client/src/components/Hero.tsx
+++ packages/client/src/components/Hero.tsx
@@ -1,14 +1,11 @@
+import Button from "./Button";
+
 export default function Hero() {
     return (
-        <div className="flex flex-col items-center my-12 gap-4">
-            <h1 className=" text-gray-800 font-bold text-4xl font-serif tracking-tight max-w-[30ch] text-center">The completely <span className="text-gray-500">Free</span> and <span className="text-gray-500">Open-Source</span> NewsAPI alternative</h1>
-            <p className="text-gray-500 text-center">Use our free API or set it up on your own machine, All goes!</p>
-
-            <div className="flex gap-2">
-            <button className="bg-gray-800 text-white px-4 py-3 font-semibold text-sm rounded-md">Get a Free API</button>
-            <button className="text-gray-800 border border-gray-800 text-white px-4 py-3 font-semibold text-sm rounded-md">Read the docs</button>
-            </div>
-
+        <div className="flex flex-col items-center mt-12 mb-10 gap-6 px-2">
+            <h1 className="text-gray-800 font-bold text-5xl font-serif tracking-tight max-w-[30ch] text-center">The completely <span className="text-gray-500">Free</span> & <span className="text-gray-500">Open-Source</span> NewsAPI alternative</h1>
+            <p className="text-gray-500 text-center max-w-[60ch] leading-normal">Use the (mostly) free and unlimited API or host it on your on machine, supports a vast catalog of continuously growing news sources</p>
+            <Button className="w-1/2 py-3 font-semibold sm:w-1/4" variant="primary">Get a Free API Key</Button>
         </div>
     )
 }
--- packages/client/src/components/Navbar.tsx
+++ packages/client/src/components/Navbar.tsx
@@ -1,18 +1,23 @@
+import Button from "./Button";
+
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
     return (
         <nav className="p-4 flex justify-between items-center max-w-5xl mx-auto">
             <span className="font-bold text-2xl">NewsMan</span>
 
             <ul className="flex gap-3 justify-center">
-                <li><a className="text-gray-500" href="#">Service</a></li>
-                <li><a className="text-gray-500" href="#">Quickstart</a></li>
-                <li><a className="text-gray-500" href="#">Examples</a></li>
+
+
+                <li> <Link to="/docs" className="text-gray-500"> Documentation </Link> </li>
+                <li><a className="text-gray-500" href="https://github.com/aadv1k/newsman" target="_blank">GitHub</a></li>
             </ul>
 
 
             <div className="flex gap-2">
-                <button className="text-gray-800  px-4 py-1 rounded-md">Login</button>
-                <button className="bg-gray-800 text-white px-4 py-1 text-sm rounded-md shadow shadow-lg">Sign Up</button>
+                <Button variant="secondary" className="shadow-none">Login</Button>
+                <Button variant="primary" className="py-2">Sign Up</Button>
             </div>
 
         </nav>
